e99a660409ebda7b862267ddf6824fa5994ec19b
Kenneth Reitz <me@kennethreitz.com>
2013-10-03 13:43:00 -0400

    js fix

--- static/js/application.js
+++ static/js/application.js
@@ -6,7 +6,7 @@ var outbox = new ReconnectingWebSocket("ws://"+ location.host + "/submit");
 
 inbox.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html(); + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html();+ "</div></div>");
+  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
   $("#chat-text").stop().animate({
     scrollTop: $('#chat-text')[0].scrollHeight
   }, 800);
