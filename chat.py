a07d2ffaeb48988dd5a07ebbfd5a85c4a7bc4a83
Kenneth Reitz <me@kennethreitz.com>
2013-10-04 10:51:03 -0400

    COMMENTS!

--- chat.py
+++ chat.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Chat Server
+===========
+
+This simple application uses WebSockets to run a primitive chat server.
+"""
 
 import os
 import logging
@@ -10,20 +18,23 @@ REDIS_URL = os.environ['REDISCLOUD_URL']
 REDIS_CHAN = 'chat'
 
 app = Flask(__name__)
-# app.debug = True
-sockets = Sockets(app)
+app.debug = 'DEBUG' in os.environ
 
+sockets = Sockets(app)
 redis = redis.from_url(REDIS_URL)
 
 
 
 class ChatBackend(object):
+    """Interface for registering and updating BebSocket clients."""
+
     def __init__(self):
         self.clients = list()
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(REDIS_CHAN)
 
     def register(self, client):
+        """Register a WebSocket connection for Redis updates."""
         self.clients.append(client)
 
     def __iter_data(self):
@@ -34,36 +45,31 @@ class ChatBackend(object):
                 yield data
 
     def send(self, client, data):
+        """Send given data to all registered clients."""
         try:
             client.send(data)
         except Exception:
             self.clients.remove(client)
 
     def run(self):
+        """Listens for new messages in Redis, and sends them to clients."""
         for data in self.__iter_data():
             for client in self.clients:
                 gevent.spawn(self.send, client, data)
 
-
     def start(self):
+        """Maintains Redis subscription in the background."""
         gevent.spawn(self.run)
 
-
-def hmm(x):
-    print x
-
 chats = ChatBackend()
-# chats.register(hmm)
 chats.start()
-# exit()
-# # chats.start()
-
 
 
 @sockets.route('/submit')
 def inbox(ws):
-
+    """Receives incoming chat messages, inserts them into Redis."""
     while ws.socket is not None:
+        # Sleep to prevent *contstant* context-switches.
         gevent.sleep(0.1)
         message = ws.receive()
 
@@ -75,40 +81,15 @@ def inbox(ws):
 
 @sockets.route('/receive')
 def outbox(ws):
-    # stream = chats.gen()
+    """Sends outgoing chat messages, via `ChatBackend`."""
     chats.register(ws)
 
-
-
     while ws.socket is not None:
-    #     ws.
-    #     for message in stream:
-    #         ws.send(stream)
+        # Context switch while `ChatBackend.start` is running in the background.
         gevent.sleep()
 
-
-
-# @sockets.route('/receive')
-# def outbox(ws):
-#     chats.register(ws)
-#     pubsub.subscribe(REDIS_CHAN)
-
-#     while ws.socket is not None:
-#         gevent.sleep(0.1)
-#         # Message receipt
-#         for message in pubsub.listen():
-
-#             data = message.get('data')
-#             if message['type'] == 'message':
-#                 app.logger.info(u'Sending message: {}'.format(data))
-#                 ws.send(data)
-
-#         # gevent.sleep(0.1)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-@app.route('/active')
-def active():
-    return len(chats.clients)
