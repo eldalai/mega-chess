c02b04141924c5de95a5e2a1b97dcec8f23773cb
Gabriel Flores <gabrielf@eventbrite.com>
2016-06-26 10:32:15 -0300

    turn control

--- manager.py
+++ manager.py
@@ -11,6 +11,10 @@ class InvalidBoardIdException(ManagerException):
     pass
 
 
+class InvalidTurnTokenException(ManagerException):
+    pass
+
+
 class Manager(object):
     '''
     Responsable to map board_id with board_id
@@ -21,6 +25,19 @@ class Manager(object):
     def __init__(self):
         super(Manager, self).__init__()
         self.boards = {}
+        self.turns = {}
+
+    def _next_turn_token(self, board_id, previous_turn_token=None):
+        if previous_turn_token and previous_turn_token in self.turns:
+            del self.turns[previous_turn_token]
+        turn_token = str(uuid.uuid4())
+        self.turns[turn_token] = board_id
+        return turn_token
+
+    def challenge(self, white_player_id, black_player_id):
+        board_id = self.create_board()
+        turn_token = self._next_turn_token(board_id)
+        return turn_token
 
     def create_board(self):
         board_id = str(uuid.uuid4())
@@ -32,6 +49,11 @@ class Manager(object):
             raise InvalidBoardIdException()
         return self.boards[board_id]
 
+    def get_board_id_by_turn_token(self, turn_token):
+        if turn_token not in self.turns:
+            raise InvalidTurnTokenException()
+        return self.turns[turn_token]
+
     def move(self, board_id, from_row, from_col, to_row, to_col):
         return self.get_board_by_id(board_id).move(
             from_row,
@@ -39,3 +61,8 @@ class Manager(object):
             to_row,
             to_col,
         )
+
+    def move_with_turn_token(self, turn_token, from_row, from_col, to_row, to_col):
+        board_id = self.get_board_id_by_turn_token(turn_token)
+        self.move(board_id, from_row, from_col, to_row, to_col)
+        return self._next_turn_token(board_id, turn_token)
--- test_manager.py
+++ test_manager.py
@@ -7,6 +7,7 @@ from pychess.chess import (
 
 from manager import (
     InvalidBoardIdException,
+    InvalidTurnTokenException,
     Manager,
 )
 
@@ -39,6 +40,32 @@ class TestManager(unittest.TestCase):
         board = self.manager.get_board_by_id(self.board_id)
         self.assertIsNotNone(board)
         self.assertEqual(board.actual_turn, BLACK)
+
+    def test_start_game(self):
+        turn_token = self.manager.challenge('user1', 'user2')
+        self.assertIsNotNone(turn_token)
+
+    def test_move_with_turn_token(self):
+        first_turn_token = self.manager.challenge('user1', 'user2')
+        # initial board turn should be WHITE
+        board_id = self.manager.get_board_id_by_turn_token(first_turn_token)
+        board = self.manager.get_board_by_id(board_id)
+        self.assertEqual(board.actual_turn, WHITE)
+        # move WHITE with token
+        second_turn_token = self.manager.move_with_turn_token(first_turn_token, 6, 3, 5, 3)
+        # second board turn should be BLACK
+        board_id = self.manager.get_board_id_by_turn_token(second_turn_token)
+        board = self.manager.get_board_by_id(board_id)
+        self.assertEqual(board.actual_turn, BLACK)
+        # invalid turn token exception
+        with self.assertRaises(InvalidTurnTokenException):
+            self.manager.move_with_turn_token(first_turn_token, 6, 3, 5, 3)
+        # move BLACK with token
+        third_turn_token = self.manager.move_with_turn_token(second_turn_token, 1, 3, 2, 3)
+        self.assertIsNotNone(third_turn_token)
+        board_id = self.manager.get_board_id_by_turn_token(third_turn_token)
+        board = self.manager.get_board_by_id(board_id)
+        self.assertEqual(board.actual_turn, WHITE)
 
 if __name__ == '__main__':
     unittest.main()
